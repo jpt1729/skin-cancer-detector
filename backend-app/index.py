e92df89e0d10009b1704d3e0bbdf298a2da9a5d8
John <136768731+jpt1729@users.noreply.github.com>
2024-07-30 13:32:48 -0700

    updates to backend

--- backend-app/index.py
+++ backend-app/index.py
@@ -1,44 +1,148 @@
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify
+import time
+from werkzeug.utils import secure_filename
 import os
-from datetime import datetime
+from PIL import Image
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-UPLOAD_FOLDER = './uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-@app.route('/binary-upload', methods=['PATCH'])
-def binary_upload():
-    print(request.files)
-    if 'file' not in request.files:
-        abort(400, description="No file part in the request")
-    
-    file = request.files['file']
-    
-    if file.filename == '':
-        abort(400, description="No selected file")
-    
-    filename = f'image{datetime.now().strftime("%Y%m%d%H%M%S%f")}.png'
-    filepath = os.path.join(UPLOAD_FOLDER, filename)
-    file.save(filepath)
-    
-    return jsonify({"message": "OK", "filename": filename})
+questions = {
+    "questions": [
+        {
+            "title": "What part of the body is this?",
+            "name": "body-part",
+            "options": [
+                {
+                    "name": "lower extremity (leg, hip, thigh, etc.)",
+                    "value": "lower-extremity"
+                },
+                {
+                    "name": "upper extremity (arm, wrist, hand, etc.)",
+                    "value": "upper-extremity"
+                },
+                {
+                    "name": "anterior torso (front chest, stomach, etc.)",
+                    "value": "anterior-torso"
+                },
+                {
+                    "name": "posterior torso (back, shoulders, etc.)",
+                    "value": "posterior-torso"
+                },
+                {
+                    "name": "head/neck",
+                    "value": "head"
+                }
+            ]
+        },
+        {
+            "title": "Age group",
+            "name": "age",
+            "options": [
+                {
+                    "name": "0-19",
+                    "value": "0-19"
+                },
+                {
+                    "name": "20-39",
+                    "value": "20-39"
+                },
+                {
+                    "name": "40-59",
+                    "value": "40-59"
+                },
+                {
+                    "name": "60-79",
+                    "value": "60-79"
+                },
+                {
+                    "name": "80-99",
+                    "value": "80-99"
+                }
+            ]
+        },
+        {
+            "title": "Sex",
+            "name": "sex",
+            "options": [
+                {
+                    "name": "male",
+                    "value": "male"
+                },
+                {
+                    "name": "female",
+                    "value": "female"
+                }
+            ]
+        }
+    ]
+}
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part in the request'}), 400
-    
-    file = request.files['file']
-    
+# Positive responses should be in the form { probability: 0.1 }, 200
+# Errors should be in the form {error: "error message here"}, 400
+
+model_details = {"resolution": {
+    "width": 256,
+    "height": 256,
+}}
+
+
+def validate_answer(param_name, param_value):
+    for question in questions["questions"]:
+        if question["name"] == param_name:
+            for option in question["options"]:
+                if option["value"] == param_value:
+                    return True
+    return False
+
+
+@app.route('/upload', methods=['PATCH'])
+def upload():
+    if 'photo' not in request.files:
+        return {"error": 'No file part'}, 400
+
+    file = request.files['photo']
+
     if file.filename == '':
-        return jsonify({'error': 'No file selected for uploading'}), 400
+        return {"error": 'No selected file'}, 400
+
+    if not file:
+        return {"error": "File does not exist"}, 400
+
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    img = Image.open(file)
+    width, height = img.size
+
+    if model_details["resolution"] != {"height": height, "width": width}:
+        return {"error": "Incorrect resolution"}, 400
+
+    body_part = request.args.get('body-part')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+
+    if not (validate_answer('body-part', body_part) and
+            validate_answer('age', age) and
+            validate_answer('sex', sex)):
+        return {"error": 'Invalid answers'}, 400
     
-    # Save the file or process it here
-    file.save(f'.//{file.filename}')
-    
-    return jsonify({'message': 'File successfully uploaded'}), 200
+    return {"probability": 0.1}, 200
+
+
+@app.route('/model-details', methods=['GET'])
+def get_model_details():
+    return jsonify(model_details), 200
+
+
+@app.route('/questions', methods=['GET'])
+def get_questions():
+    return jsonify(questions), 200
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=3000)
---
+++ backend-app/run.bat
@@ -0,0 +1,2 @@
+set FLASK_DEBUG=1
+py -m flask --app api/index run -p 5000 --host=0.0.0.0
