465c60e7343d8f2da8cf8312e114c2ec2789f5ef
John <136768731+jpt1729@users.noreply.github.com>
2024-08-02 11:31:47 -0700

    borking

--- backend-app/utils.py
+++ backend-app/utils.py
@@ -62,7 +62,7 @@ def load_image(file):
 
 
 def model_predict(img, txt):
-    model = tf.keras.models.load_model("./models/new-model.keras")
-    prediction = model.predict(img)
-    prediction = prediction[0]
+    model = tf.keras.models.load_model("./models/multimodal-base.keras")
+    prediction = model.predict([img, txt])
+    prediction = 1 - prediction[0]
     return prediction
